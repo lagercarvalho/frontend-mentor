6854aca41353fbb1d7344d86d6f8bf756a0e5dd4
Alexander Carvalho <lagercarvalho@gmail.com>
2023-09-17 18:37:47 +0200

    Done with expenses chart challenge

--- expenses-chart/import-json.js
+++ expenses-chart/import-json.js
@@ -16,6 +16,19 @@ var columns = document.querySelectorAll(".chart-column");
 columns.forEach(function(column,i) {
   console.log(amountRatio * file[i].amount);
   const height = amountRatio * file[i].amount;
-  column.style.height = height + "px";
+  animateColumn(column, height);
   column.querySelector("span").innerHTML = "$" + file[i].amount
 });
+
+function animateColumn(column, endValue) {
+  let height = 0;
+  let counter = setInterval(function () {
+    console.log(height)
+    if (height >= endValue) {
+      clearInterval(counter);
+    } else {
+      height += 1;
+    }
+    column.style.height = height + "px";
+  });
+}
